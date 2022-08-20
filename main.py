4c502c93508037e6f122795fbf22a2afe41cc978
Purin Sukpanichnant <ps1620@ic.ac.uk>
2022-08-20 14:17:26 +0100

    Add SHAP explanations to male and female pages

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 
 from utils import calculateOutputsMale, calculateAllDataMale, calculateOutputsFemale, calculateAllDataFemale, male_labels, female_labels
 
+from shap_exp import male_base_risks, female_base_risks, calculateShapValuesMale, calculateShapValuesFemale
+
 app = Flask(__name__)
 
 app.config["JSON_SORT_KEYS"] = False
@@ -12,19 +14,23 @@ app.secret_key = "B6i6zsH!C6ez"
 def male():
     outputs = None
     all_data = None
+    male_shap_values = None
     if request.method == 'POST':
         outputs = calculateOutputsMale(request.form)
         all_data = calculateAllDataMale(request.form, outputs)
-    return render_template('male.html', outputs=outputs, all_data=all_data, labels=male_labels)
+        male_shap_values = calculateShapValuesMale(request.form)
+    return render_template('male.html', outputs=outputs, all_data=all_data, labels=male_labels, base_risks=male_base_risks, shap_values=male_shap_values)
 
 @app.route('/female', methods=['GET', 'POST'])
 def female():
     outputs = None
     all_data = None
+    female_shap_values = None
     if request.method == 'POST':
         outputs = calculateOutputsFemale(request.form)
         all_data = calculateAllDataFemale(request.form, outputs)
-    return render_template('female.html', outputs=outputs, all_data=all_data, labels=female_labels)
+        female_shap_values = calculateShapValuesFemale(request.form)
+    return render_template('female.html', outputs=outputs, all_data=all_data, labels=female_labels, base_risks=female_base_risks, shap_values=female_shap_values)
 
 if __name__ == "__main__":
     app.run(debug=True)
